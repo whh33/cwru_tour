8cfb3e95d6fa24a2e9c40f9be8da5fb8c03970ea
Heath Hudgins <whh33@case.edu>
2014-02-23 21:51:49 -0500

    Added a new table view

---
+++ cwru_tour/cwru_tour/BuildingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  BuildingsViewController.h
+//  cwru_tour
+//
+//  Created by Heath Hudgins on 2/23/14.
+//  Copyright (c) 2014 Heath Hudgins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BuildingsViewController : UITableViewController
+
+@end
